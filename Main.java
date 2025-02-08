201f66088ef07b38dd7a370b3efbb0eff3256a6f
IanRMarques <99884736+IanRMarques@users.noreply.github.com>
2025-02-07 22:45:32 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Ola Java ");
+        int x =20;
+        int z = 2131;
+        System.out.println("O valor de Z é: " + z);
+
+
+    }
+}
